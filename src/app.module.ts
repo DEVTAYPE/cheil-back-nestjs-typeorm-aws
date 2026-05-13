60c92ea8d2e5e1bc8c685ac9e45958a3eae8965a
taypedev <jetaypedev@gmail.com>
2026-05-12 21:27:42 -0500

    chore(config): setup env variables with Joi validation and API versioning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().min(32).required(),
+        JWT_EXPIRES_IN: Joi.string().default('1d'),
+        CORS_ORIGINS: Joi.string().default('http://localhost:3001'),
+      }),
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
